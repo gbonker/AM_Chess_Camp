cbc99a101c821b018dcacc275ea2e8a177302b1f
Gale Bonker <gbonker@andrew.cmu.edu>
2014-05-01 10:27:41 -0400

    LOG IN WORKS YAY

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :role, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :username, :role, :password, :password_confirmation, :active)
   end
 
 end
