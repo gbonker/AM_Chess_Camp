c80a4fc9349f9e8f8500270bf22382697fffc6cd
Gale Bonker <gbonker@andrew.cmu.edu>
2014-05-06 16:39:34 -0400

    working on dashboards

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 	
   def index
+  	@available_camps = Camp.upcoming.active.chronological.select{ |camp| camp.registrations.size < camp.location.max_capacity }.take(10)
   end
 
   def about
