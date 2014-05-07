565ef20c3f51835f55539250725d924ba7e0b577
Gale Bonker <gbonker@andrew.cmu.edu>
2014-05-07 16:58:25 -0400

    fixed some things, now my maps don't work :'(

--- app/models/location.rb
+++ app/models/location.rb
@@ -21,6 +21,11 @@ class Location < ActiveRecord::Base
   before_destroy :verify_that_never_used_for_camps
   #before_validation :get_location_coordinates
 
+  def create_map_link(zoom=12,width=250,height=250)
+    marker = "&markers=color:red%7Ccolor:red%7Clabel:#{i}%7C#{self.latitude},#{self.longitude}"
+    map = "http://maps.google.com/maps/api/staticmap?center= #{latitude},#{longitude}&zoom=#{zoom}&size=#{width}x#{height}&maptype=roadmap#{marker}&sensor=false"
+  end
+
   private
   def verify_that_never_used_for_camps
     self.camps.empty?
